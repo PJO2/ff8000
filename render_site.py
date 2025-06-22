6d01f7858bb3dee3a86059815287fdd3a8329fd7
PJO <pjo@ff8000.net>
2025-06-22 20:03:45 +0200

    update blog page blog data moved into the json files
    Re-add tiles to the english html page in data.tiles

--- render_site.py
+++ render_site.py
@@ -9,8 +9,9 @@ import json
 import os
 
 # go to shared directory
-os.chdir ('/media/sf_ff8000/promotion/web_site/ff8000')
+os.chdir ('/media/sf_homes/ff8000/promotion/web_site/ff8000')
 
+# Site pages
 PAGES = [ 'accueil', 'blog', 'certifs', 'contact', 'references', 'services' ]
 LANGUAGES = [ 'en', 'fr' ]
 # LANGUAGES = [ 'fr' ]
