ba784c9952dda87daca4683ab143f1dd064b9a5f
PJO <pjo@ff8000.net>
2025-06-22 20:03:45 +0200

    update blog page

--- render_site.py
+++ render_site.py
@@ -9,7 +9,7 @@ import json
 import os
 
 # go to shared directory
-os.chdir ('/media/sf_ff8000/promotion/web_site/ff8000')
+os.chdir ('/media/sf_ff8000/')
 
 PAGES = [ 'accueil', 'blog', 'certifs', 'contact', 'references', 'services' ]
 LANGUAGES = [ 'en', 'fr' ]
